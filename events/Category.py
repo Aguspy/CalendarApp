45bcce357c13af25788d83e16d6523415f0e9699
Aguspy <janifu02@gmail.com>
2020-10-13 14:07:51 +0200

    constants of the Events

---
+++ events/Category.py
@@ -0,0 +1,26 @@
+import json
+class Category:
+    categories=dict()
+    default_categories_file="categories.json"
+
+    @classmethod
+    def createSuggestedCategory(cls):
+        standardcategories = {
+            "important and urgent" = 0
+            "not important and urgent" = 1
+            "important and not urgent" = 2
+            "neither" = 3
+            "fixed" = 4
+        }        
+        cls.categories.update(standardcategories)
+        cls.write_categories_to_file()
+    
+    @classmethod
+    def createOwnCategory(cls, anotherCategory, categories):
+        cls.categories[anotherCategory] = len(cls.categories)
+        cls.write_categories_to_file()
+        
+    @classmethod
+    def write_categories_to_file(file_name=default_categories_file)
+        with open(file_name,'w') as f:
+            json.dump(cls.categories,f,indent=2)
